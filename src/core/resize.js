dd98efb48fd91735fcf03a15869307c0d52265eb
Dennis Stockhofe <d.stockhofe@shopmacher.de>
2018-03-12 13:28:33 +0100

    Adds documentation, removes useless null check

--- src/core/resize.js
+++ src/core/resize.js
@@ -1,11 +1,17 @@
 import scale from './scale';
 
+/**
+ * Resizes the image fit to the container
+ *
+ * @param $container
+ * @param store
+ */
 export default ($container, store) => {
     const { options } = store.getState();
 
     Array.prototype.forEach.call($container, $element => {
         const type = $element.parentNode.classList.contains('lightbox-slider--main') ? 'main' : 'thumb';
-        const limiter = type === 'thumb' && options.thumbSlider && options.thumbSlider.visibleSlides ? options.thumbSlider.visibleSlides : null;
+        const limiter = type === 'thumb' && options.thumbSlider.visibleSlides ? options.thumbSlider.visibleSlides : null;
         const $images = $element.querySelectorAll('img');
         const elementWidth = limiter ? $element.getBoundingClientRect().width / limiter : $element.getBoundingClientRect().width;
         const elementHeight = $element.getBoundingClientRect().height;
